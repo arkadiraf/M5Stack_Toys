222d7d6427d06a9ad9e40d48a0f166a98835e230
Arkadiraf <arkadiraf@gmail.com>
2024-04-03 15:42:56 +0300

    Update m5stickc_wifi.ino

--- IMU_Wifi/m5stickc_wifi/m5stickc_wifi.ino
+++ IMU_Wifi/m5stickc_wifi/m5stickc_wifi.ino
@@ -1,83 +1,38 @@
-#include <M5StickCPlus.h>
-#include <WiFi.h>
+// Wire Slave Receiver
+// by Nicholas Zambetti <http://www.zambetti.com>
 
-unsigned long lastMeasurementTime = 0; // Last measurement time
+// Demonstrates use of the Wire library
+// Receives data as an I2C/TWI slave device
+// Refer to the "Wire Master Writer" example for use with this
 
-const char* ssid = "Controllab_Projects";
-const char* password = "Controllab";
-WiFiClient client;
-const char* serverIP = "192.168.0.103"; // IP address of the server
-const int serverPort = 60691; // Port number the server is listening on
+// Created 29 March 2006
 
-struct Measurement {
-  float accX, accY, accZ;
-  float gyroX, gyroY, gyroZ;
-  bool button;
-  unsigned long timestamp;
-};
+// This example code is in the public domain.
 
-Measurement measurements[10]; // Buffer for 10 measurements
-int measurementIndex = 0; // Current index in the buffer
 
-void setup() {
-  M5.begin();
-  M5.Lcd.setRotation(3);
-  M5.Lcd.setCursor(50, 15);
-  M5.Lcd.println("Connecting to WiFi...");
-  M5.Imu.Init();
-  WiFi.begin(ssid, password);
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print(".");
-  }
+#include <Wire.h>
 
-  Serial.println("\nConnected to WiFi");
-  M5.Lcd.println("Connected to WiFi");
-  M5.Lcd.println(" X    Y    Z");
-  Serial.begin(115200);
+void setup()
+{
+  Wire.begin(4);                // join i2c bus with address #4
+  Wire.onReceive(receiveEvent); // register event
+  Serial.begin(9600);           // start serial for output
 }
 
-void loop() {
-  M5.update();
-  if (!client.connected()) {
-    M5.Lcd.fillScreen(BLACK);
-    M5.Lcd.setCursor(50, 15);
-    M5.Lcd.println("Reconnecting  server...");
-    if (client.connect(serverIP, serverPort)) {
-      M5.Lcd.println("Reconnected to server!");
-    } else {
-      M5.Lcd.println("Failed to reconnect to server.");
-      return; // Don't proceed if we're not connected
-    }
+void loop()
+{
+  delay(100);
+}
+
+// function that executes whenever data is received from master
+// this function is registered as an event, see setup()
+void receiveEvent(int howMany)
+{
+  while(1 < Wire.available()) // loop through all but the last
+  {
+    char c = Wire.read(); // receive byte as a character
+    Serial.print(c);         // print the character
   }
-
-  // measure intervals:
-  unsigned long currentTime = millis();
-  if (currentTime - lastMeasurementTime >= 10) { // Check if more than 10 ms have passed
-    lastMeasurementTime = currentTime;
-    M5.Imu.getGyroData(&measurements[measurementIndex].gyroX, &measurements[measurementIndex].gyroY, &measurements[measurementIndex].gyroZ);
-    M5.Imu.getAccelData(&measurements[measurementIndex].accX, &measurements[measurementIndex].accY, &measurements[measurementIndex].accZ);
-    measurements[measurementIndex].button = M5.BtnA.read();
-    measurements[measurementIndex].timestamp = millis();
-
-    measurementIndex++;
-
-    // When buffer is full, send all measurements in one large buffer
-    if (measurementIndex >= 10) {
-      String dataToSend;
-      for (int i = 0; i < 10; i++) {
-        dataToSend += "IMU:" + String(measurements[i].timestamp) + "," +
-                      String(measurements[i].accX, 3) + "," +
-                      String(measurements[i].accY, 3) + "," +
-                      String(measurements[i].accZ, 3) + "," +
-                      String(measurements[i].gyroX, 2) + "," +
-                      String(measurements[i].gyroY, 2) + "," +
-                      String(measurements[i].gyroZ, 2) + "," +
-                      String(measurements[i].button ? "1" : "0") + "\r\n";
-      }
-      client.print(dataToSend); // Send the entire string in one go
-      measurementIndex = 0; // Reset buffer index
-    }
-  }
-
+  int x = Wire.read();    // receive byte as an integer
+  Serial.println(x);         // print the integer
 }
